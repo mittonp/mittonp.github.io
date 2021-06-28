3eb7d2e2ccae1305498cbad208c35b430ad492c4
Mitton Pienaar <mitton.pienaar@outlook.com>
2021-06-28 10:07:39 +0200

    updates

--- lebara/main.js
+++ lebara/main.js
@@ -13,9 +13,30 @@ container.height = am4core.percent(100);
 var chart = container.createChild(am4maps.MapChart);
 chart.width = am4core.percent(100);
 chart.height = am4core.percent(100);
+//chart.height = 205;
 chart.zoomControl = new am4maps.ZoomControl();
 chart.zoomControl.align = "right";
+chart.zoomControl.valign = "bottom";
+chart.zoomControl.minusButton.background.cornerRadiusTopLeft = 0;
+chart.zoomControl.minusButton.background.cornerRadiusTopRight = 0;
+chart.zoomControl.minusButton.background.cornerRadiusBottomRight =
+  am4core.percent(100);
+chart.zoomControl.minusButton.background.cornerRadiusBottomLeft =
+  am4core.percent(100);
+chart.zoomControl.plusButton.background.cornerRadiusBottomLeft = 0;
+chart.zoomControl.plusButton.background.cornerRadiusBottomRight = 0;
+chart.zoomControl.plusButton.background.cornerRadiusTopRight =
+  am4core.percent(100);
+chart.zoomControl.plusButton.background.cornerRadiusTopLeft =
+  am4core.percent(100);
 
+chart.zoomControl.minusButton.background.fillOpacity = 0;
+chart.zoomControl.plusButton.background.fillOpacity = 0;
+chart.zoomControl.minusButton.background.stroke = am4core.color("#c4c4c4");
+chart.zoomControl.plusButton.background.stroke = am4core.color("#c4c4c4");
+
+chart.zoomControl.minusButton.fontWeight = "bold";
+chart.zoomControl.plusButton.fontWeight = "bold";
 // var ttcontainer = am4core.create("mobiletooltip", am4core.Container);
 // ttcontainer.layout = "vertical";
 // ttcontainer.width = am4core.percent(100);
@@ -59,27 +80,23 @@ $.getJSON("country-data.json", function (data) {
 // Make map load polygon (like country names) data from GeoJSON
 polygonSeries.useGeodata = true;
 
-tooltipTemplate =
-  "<div class='am-tooltip'>" +
-  "<div class='country-name'>{name}</div>" +
-  "<div class='text'>{text}</div>" +
-  "<div id='product-section-wrapper'>" +
-  "<div class='sim-plans-content-wrapper'>" +
-  " <div class='each-sim-plans-wrap border-radius20'>" +
-  "   <div class='espw-inner'>" +
-  "     <div class='d-inline-block custom-width'>" +
-  "       <h3>Best Plan</h3>" +
-  "<p><strong>{bestPlan}</strong> {bestPlanMinutes}</p>" +
-  "<div class='price-wrap'><h3>{bestPlanPrice}</h3></div>" +
-  "       </div>" +
-  "<div class='btn-wrap'>" +
-  "       <a href='buy'><button class='btn btn-primary buynow'>Buy SIM</button></a>" +
-  "       </div>" +
-  "       </div>" +
-  "       </div>" +
-  "       </div>" +
-  "       </div>" +
-  "</div>";
+tooltipTemplate = `
+<div class='am-tooltip'>
+  <div class='country-name'>{name}</div>
+  <div class='text'>{text}</div>
+  <div class='am-tooltip__plan-wrapper'>
+  <div class='am-tooltip__plan'>
+    <div class='best-plan__heading'>Best Plan</div>
+    <p><strong class="best-plan__name">{bestPlan}</strong> <span class="best-plan__minutes">{bestPlanMinutes}</span>
+    </p>
+    <div class='price-wrap'>
+      <div class="best-plan__price">{bestPlanPrice}</div>
+    </div>
+    <a href='buy'><button class='btn btn-primary buynow'>Buy SIM</button></a>
+  </div>
+  </div>
+</div>
+  `;
 
 // Configure series
 polygonSeries.calculatePercent = true;
@@ -96,10 +113,20 @@ if (!wideScreen) {
   var info = container.createChild(am4core.Label);
   info.align = "center";
   //info.width = am4core.percent(100);
+  info.tooltip.label.interactionsEnabled = true;
   info.tooltip.getFillFromObject = false;
   info.tooltip.background.fill = am4core.color("#ffffff");
   info.tooltip.background.fillOpacity = 0.9;
   info.tooltip.ignoreBounds = true;
+  info.tooltip.background.cornerRadius = 20;
+  info.tooltip.background.pointerBaseWidth = 40;
+  info.tooltip.background.pointerLength = 20;
+  var shadow = info.tooltip.background.filters.getIndex(0);
+  shadow.dx = 3;
+  shadow.dy = 6;
+  shadow.blur = 46;
+  shadow.color = am4core.color("#000");
+  info.tooltip.label.padding(0, 0, 0, 0);
 
   info.tooltip.events.on("hit", function (ev) {
     console.log(ev);
@@ -115,7 +142,7 @@ if (!wideScreen) {
 }
 
 //Set up tooltips
-polygonSeries.calculateVisualCenter = false;
+polygonSeries.calculateVisualCenter = true;
 polygonTemplate.tooltipPosition = "fixed";
 polygonSeries.tooltip.label.interactionsEnabled = true;
 polygonSeries.tooltip.keepTargetHover = true;
@@ -123,6 +150,15 @@ polygonSeries.tooltip.pointerOrientation = "horizontal";
 polygonSeries.tooltip.getFillFromObject = false;
 polygonSeries.tooltip.background.fill = am4core.color("#ffffff");
 polygonSeries.tooltip.background.fillOpacity = 0.9;
+polygonSeries.tooltip.background.cornerRadius = 20;
+polygonSeries.tooltip.background.pointerBaseWidth = 40;
+polygonSeries.tooltip.background.pointerLength = 20;
+var shadow = polygonSeries.tooltip.background.filters.getIndex(0);
+shadow.dx = 3;
+shadow.dy = 6;
+shadow.blur = 46;
+shadow.color = am4core.color("#000");
+polygonSeries.tooltip.label.padding(0, 0, 0, 0);
 
 // Create hover state and set alternative fill color
 var hs = polygonTemplate.states.create("hover");
