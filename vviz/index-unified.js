99ac50280199926b3d98ff2da7e880cda8a5e73a
Mitton Pienaar <mitton.pienaar@outlook.com>
2021-07-12 04:37:00 +0200

    reset scroll BEFORE switching views

--- vviz/index-unified.js
+++ vviz/index-unified.js
@@ -1,16 +1,16 @@
 var skrollrInstance = skrollr.init();
 
 $(".progress-indicator__bullet").click(function (e) {
+  skrollrInstance.setScrollTop(0);
   var target = e.target.attributes["data-target"].nodeValue;
   var element = $(target);
   var sections = $(".section-canvas");
   sections.hide();
   element.show();
-
   var progressIndicators = $(".progress-indicator__bullet");
   progressIndicators.removeClass("progress-indicator__bullet--active");
   $(e.target).addClass("progress-indicator__bullet--active");
-  skrollrInstance.setScrollTop(0);
+
   console.log(target);
 });
 
