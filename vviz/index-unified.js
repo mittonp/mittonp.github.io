5f4693bf98e7003a3224b2281aa54a43d9cf9bc0
Mitton Pienaar <mitton.pienaar@outlook.com>
2021-07-12 04:27:20 +0200

    skrollr instance for page top

--- vviz/index-unified.js
+++ vviz/index-unified.js
@@ -13,7 +13,8 @@ $(".progress-indicator__bullet").click(function (e) {
   var progressIndicators = $(".progress-indicator__bullet");
   progressIndicators.removeClass("progress-indicator__bullet--active");
   $(e.target).addClass("progress-indicator__bullet--active");
-  s.setScrollTop(0);
+  var ss = skrollr.init();
+  ss.setScrollTop(0);
   console.log(target);
 });
 
