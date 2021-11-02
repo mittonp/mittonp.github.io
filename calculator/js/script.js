ba23d68fa895d064ad155e01dc26a31aa0194e12
EFEGray <dong.trinh@efe.com.vn>
2021-11-02 11:59:32 +0700

    Update Js

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -258,11 +258,64 @@ jQuery(document).ready(function () {
     var chunk = btoa(JSON.stringify(model));
     console.log(chunk);
     var shareUrl = window.location.href.split('?')[0] + "?m=" + chunk;
-    console.log(shareUrl);
+    var shortUrl = shortenUrl(shareUrl);
+    console.log(shortUrl);
     navigator.clipboard.writeText(shareUrl);
     window.alert("Link copied");
 
   })
+
+  const ENDPOINT = 'https://www.jsonstore.io/b7500b390a31a7fdc2bde29c94607fda2a38bcc19368e99835309de795232577';
+  const URL_REGEXP = new RegExp("^(http|https|ftp)://", "i");
+
+  function getRandomString() {
+    let random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
+    return random_string;
+  }
+
+  // function getUrl() {
+  //   let url = document.getElementById('txtUrl').value;
+  //   let isProtocolOk = URL_REGEXP.test(url);
+  //   return isProtocolOk ? url : ('http://' + url);
+  // }
+
+  function genHash() {
+    if (window.location.hash == '') {
+      window.location.hash = getRandomString();
+    }
+  }
+
+  function sendRequest(url) {
+    $.ajax({
+      'url': ENDPOINT + '/' + window.location.hash.substr(1) /* remove # */,
+      'type': 'POST',
+      'data': JSON.stringify(url),
+      'dataType': 'json',
+      'contentType': 'application/json; charset=utf-8'
+    })
+  }
+
+  function shortenUrl(url) {
+    let longUrl = url;
+    genHash();
+    sendRequest(longUrl);
+  }
+
+  function checkCurentUrl() {
+    let hashExisting = window.location.hash.substr(1);
+    if (hashExisting) {
+      $.getJSON(ENDPOINT + "/" + hashExisting,
+          function (data) {
+            data = data["result"];
+            if (data != null) {
+              window.location.href = data;
+            }
+          }
+      )
+    }
+  }
+
+  checkCurentUrl();
 
 
   var data = [];
