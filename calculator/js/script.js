d09dddb8de5e5bf9c9c92fe2eacc7d336b32cdee
EFEGray <dong.trinh@efe.com.vn>
2021-11-02 22:27:35 +0700

    Update JS

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -282,6 +282,10 @@ jQuery(document).ready(function () {
     });
   }
 
+  $.getScript("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js", function() {
+    alert("Script loaded but not necessarily executed.");
+  });
+
 
   var data = [];
 
